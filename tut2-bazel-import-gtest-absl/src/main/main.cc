05353be703d595b2b1663b9cab38c1b64d3bc8d6
Iliaromanov <iliaromanov033@gmail.com>
2024-01-06 11:17:30 -0500

    comments and building+runnning main

--- tut2-bazel-import-gtest-absl/src/main/main.cc
+++ tut2-bazel-import-gtest-absl/src/main/main.cc
@@ -1,8 +1,19 @@
+#include "src/lib/cpplib.h"
 #include <iostream>
 #include <string>
 
+
+/*
+To build+run this using bazel based on the src/main/BUILD do:
+
+bazel run src/main:main
+*/
+
+
 int main() {
-  std::cout << "Hello world!" << std::endl;
+  CPPLib util;
+
+  std::cout << util.PrintHelloWorld() << std::endl;
 
   return 0;
 }
